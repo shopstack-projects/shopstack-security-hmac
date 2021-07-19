87a7ca3787320fd185f63cf8dbc8775c69f0e04b
Alan Guerin <alan@alanguerin.com>
2021-07-19 19:53:16 +0100

    Add Maven Publish plugin with POM configuration.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,8 @@ plugins {
     id("io.freefair.lombok") version Versions.lombokPlugin
     checkstyle
     jacoco
+    `maven-publish`
 }
-
-group = "dev.shopstack.security"
-version = "0.1.0-SNAPSHOT"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_11
@@ -45,13 +43,63 @@ tasks {
         failFast = true
     }
 
-    jar {
-        from(sourceSets.main.get().allSource)
-    }
-
     jacocoTestReport {
         reports {
             xml.required.set(true)
+        }
+    }
+
+    val sourcesJar by creating(Jar::class) {
+        archiveClassifier.set("sources")
+        from(sourceSets.main.get().allSource)
+    }
+
+    val javadocJar by creating(Jar::class) {
+        dependsOn.add(javadoc)
+        archiveClassifier.set("javadoc")
+        from(javadoc)
+    }
+
+    artifacts {
+        archives(sourcesJar)
+        archives(javadocJar)
+        archives(jar)
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            groupId = "dev.shopstack.security"
+            artifactId = "shopstack-security-hmac"
+            version = "1.0.0"
+
+            from(components["java"])
+
+            pom {
+                name.set("Shopstack Security HMAC")
+                description.set("A library to authenticate Shopify requests using the provided HMAC.")
+                url.set("https://shopstack.dev")
+                scm {
+                    connection.set("scm:git:git://github.com/shopstack-projects/shopstack-security-hmac.git")
+                    developerConnection.set("scm:git:ssh://github.com/shopstack-projects/shopstack-security-hmac.git")
+                    url.set("https://github.com/shopstack-projects/shopstack-security-hmac/")
+                }
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                        distribution.set("repo")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("alanguerin")
+                        name.set("Alan Guerin")
+                        email.set("alan@alanguerin.com")
+                    }
+                }
+            }
         }
     }
 }
