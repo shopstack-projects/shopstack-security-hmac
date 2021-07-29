9e638c6c62df4621f243a091b690d75930bda564
Alan Guerin <alan@alanguerin.com>
2021-07-29 02:34:29 +0100

    Maven Publish (#8)
    * Add Maven Publish plugin with POM configuration.
    
    * Add publish workflow, using Sonatype OSS distribution repository.
    
    * Change publish workflow to trigger on push, for testing.
    
    * Sign artifacts with PGP key before publishing.
    
    * Force signing step for testing.
    
    * Use in-memory ascii-armored keys for signing artifact.
    
    * Base64 decode signing passphrase.
    
    * Add release.published trigger for Github Actions.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,22 @@
+import java.util.*
+
 plugins {
     java
     id("io.freefair.lombok") version Versions.lombokPlugin
     checkstyle
     jacoco
+    `maven-publish`
+    signing
 }
 
-group = "dev.shopstack.security"
-version = "0.1.0-SNAPSHOT"
+version = "1.0.0"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
+
+    withSourcesJar()
+    withJavadocJar()
 }
 
 repositories {
@@ -37,6 +43,53 @@ jacoco {
     toolVersion = Versions.jacoco
 }
 
+publishing {
+
+    publications {
+        create<MavenPublication>("mavenJava") {
+            groupId = "dev.shopstack.security"
+            artifactId = "shopstack-security-hmac"
+
+            from(components["java"])
+
+            pom {
+                name.set("Shopstack Security HMAC")
+                description.set("Authenticate Shopify requests using the provided HMAC.")
+                url.set("https://shopstack.dev")
+                scm {
+                    connection.set("scm:git:git://github.com/shopstack-projects/shopstack-security-hmac.git")
+                    developerConnection.set("scm:git:ssh://github.com/shopstack-projects/shopstack-security-hmac.git")
+                    url.set("https://github.com/shopstack-projects/shopstack-security-hmac/tree/main")
+                }
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                        distribution.set("repo")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("alanguerin")
+                        name.set("Alan Guerin")
+                        email.set("hello@alanguerin.com")
+                    }
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            name = "SonatypeOSS"
+            val sonatypeBaseUri = "https://s01.oss.sonatype.org/content/repositories"
+            val releasesRepo = uri("$sonatypeBaseUri/releases")
+            val snapshotsRepo = uri("$sonatypeBaseUri/snapshots")
+            url = if (isReleaseBuild()) releasesRepo else snapshotsRepo
+        }
+    }
+}
+
 tasks {
     test {
         useJUnitPlatform {
@@ -45,13 +98,28 @@ tasks {
         failFast = true
     }
 
-    jar {
-        from(sourceSets.main.get().allSource)
-    }
-
     jacocoTestReport {
         reports {
             xml.required.set(true)
         }
     }
+
+    signing {
+        isRequired = isReleaseBuild()
+
+        val signingKey: String? by project
+        val signingPassword: String? by project
+
+        if (signingKey != null) {
+            useInMemoryPgpKeys(base64Decode(signingKey), base64Decode(signingPassword))
+            sign(publishing.publications["mavenJava"])
+        }
+    }
+
 }
+
+fun isReleaseBuild() =
+    version.toString().contains("RELEASE")
+
+fun base64Decode(s: String?) =
+    s?.let { String(Base64.getDecoder().decode(it)).trim() }
